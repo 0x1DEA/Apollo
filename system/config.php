6009d90d1d6a7fcd6275dfacedd6e4b165d01d81
SeeBeyond <mrmajesticsandwich@gmail.com>
2020-02-03 10:23:55 -0600

    :beers: Who are you what have you sacrificed

--- system/config.php
+++ system/config.php
@@ -2,7 +2,7 @@
 defined('FEBRUARY') or header("Location: /404");
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '!JCSuperstart69420!');
 define('DB_NAME', 'apollo');
 define('DB_CREDENTIALS', array(DB_HOST, DB_USER, DB_PASS, DB_NAME));
 define('SYS_DIR', __DIR__);
